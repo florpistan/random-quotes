296512ca3491551ace83b6f7720bb8f2abe9708d
Flor Pistán Esparza <flor.pistan.esparza@gmail.com>
2021-06-17 23:28:40 -0300

    🎲 some new style details

--- app.js
+++ app.js
@@ -4,28 +4,26 @@ const quotes = [
     author: 'Elvira Sastre',
   },
   {
-    quote:
-      'Pero mis ojos son también dos tristes idiotas. No se dan cuenta de que no eres tú la que tienes que marcharte para que ellos te dejen de ver. Son ellos los que tienen que dejar de mirarte para conseguir no verte más.',
+    quote: 'Sé que me haces feliz porque mi tristeza no te reconoce.',
     author: 'Elvira Sastre',
   },
   {
     quote:
-      'Como el que lleva al mar a rastras en los ojos, el que camina hacia delante, acariciando espaldas, o el que besa parpados para soñar más tranquilo, te llevo en mi como en un accidente, hecho llanura, como una caricia que termina en poemas mientras tu duermes, como tiene el perdón grabado en el pecho el más culpable.',
+      'Que tus intentos de quebrarme el paso sólo consiguieron hacerme pisar más fuerte, y cuanto más lejos te colocas más cerca estoy de mí misma.',
     author: 'Elvira Sastre',
   },
   {
     quote:
-      'No toques si duele, amor, que una herida de tu mano es como una primavera helada y este cuerpo tirita con un sólo roce.',
+      'Me pregunto si abrá respuestas en sus ojos, si acaso piensa en quién es, si sabrá que en su mirada está mi vida completa.',
+    author: 'Elvira Sastre',
+  },
+  {
+    quote: 'Odio el frío: solo es una excusa para llamar a tu abrazo.',
     author: 'Elvira Sastre',
   },
   {
     quote:
-      'El aleteo de una mariposa en Vietnam puede causar un terremoto al otro lado del mundo. El casi imperceptible pestañeo de tus ojos es causa del fin de desastres de los míos.',
-    author: 'Elvira Sastre',
-  },
-  {
-    quote:
-      'Que la vida puede durar un cruce de miradas en medio de una tormenta. Y os aseguro que eso es un regalo, eso es más que suficiente.',
+      'No sé quién soy ni en qué baso lo que hago, pero habría tasado tu risa en una eternidad.',
     author: 'Elvira Sastre',
   },
   {
@@ -38,6 +36,10 @@ const quotes = [
       'Posees una risa capaz de rescatar al país y la mirada de quien sabe soñar con los ojos abiertos.',
     author: 'Elvira Sastre',
   },
+  {
+    quote: 'Huir significa ir a buscarte.',
+    author: 'Elvira Sastre',
+  },
 ];
 
 function newQuote() {
@@ -47,4 +49,3 @@ function newQuote() {
 }
 
 newQuote();
-  
